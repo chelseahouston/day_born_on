185f60cfc762598c4aa666405ee13191fb135c91
Chelsea Houston <chelseahouston29@gmail.com>
2021-01-07 16:34:52 +0000

    added section that calculates star sign too (additional - for fun!)

--- main.rb
+++ main.rb
@@ -9,6 +9,34 @@ D = gets.to_i
 
 puts "Your birthday is #{D}/#{month}/#{year}."
 
+#additional star sign info for fun!
+if month == 3 && D >= 21 || month == 4 && D <= 19
+  SS = "Aries"
+elsif month == 4 && D >= 20 || month == 5 && D <= 20
+  SS = "Taurus"
+elsif month == 5 && D >= 21 || month == 6 && D <= 20
+  SS = "Gemini"
+elsif month == 6 && D >= 21 || month == 7 && D <= 22
+  SS = "Cancer"
+elsif month == 7 && D >= 23 || month == 8 && D <= 22
+  SS = "Leo"
+elsif month == 8 && D >= 23 || month == 9 && D <= 22
+  SS = "Virgo"
+elsif month == 9 && D >= 23 || month == 10 && D <= 22
+  SS = "Libra"
+elsif month == 10 && D >= 23 || month == 11 && D <= 21
+  SS = "Scorpio"
+elsif month == 11 && D >= 22 || month == 12 && D <= 21
+  SS = "Sagittarius"
+elsif month == 12 && D >= 22 || month == 1 && D <= 19
+  SS = "Capricorn"
+elsif month == 1 && D >= 20 || month == 2 && D <= 18
+  SS = "Aquarius"
+elsif month == 2 && D >= 19 || month == 3 && D <= 20
+  SS = "Pisces"
+end 
+## end of additional star sign info
+
 if month <=2
   year = (year - 1)
   month = (month + 12)
@@ -47,4 +75,22 @@ elsif (R == 6)
 end
 
 puts "You were born on a #{day}."
+puts "Your star sign is #{SS}."
+
+
+# additional for fun! star sign dates ---
+
+# Aries dates: March 21 – April 19
+# Taurus dates: April 20 – May 20
+# Gemini dates: May 21 – June 20
+# Cancer dates: June 21 – July 22
+# Leo dates: July 23 – August 22
+# Virgo dates: August 23 – September 22
+# Libra dates: September 23 – October 22
+# Scorpio dates: October 23 – November 21
+# Sagittarius dates: November 22 – December 21
+# Capricorn dates: December 22 – January 19
+# Aquarius dates: January 20 – February 18
+#Pisces dates:February 19 – March 20
+
 
